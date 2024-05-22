a69a3deef7174751a0c2d85778351dd86ef72a56
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-05-22 18:19:25 -0500

    updated colums in all the models

--- models/Tag.js
+++ models/Tag.js
@@ -7,8 +7,14 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     tag_name: {
-      type: DataTypes.String,
+      type: DataTypes.STRING,
     }
   },
   {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,70 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{ 
+    const categoriesData = await Category.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(categoriesData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const singleCategory = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(singleCategory);
+  }catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const newCategory = await Category.create(req.body)
+    res.status(200).json(newCategory);
+    } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  }).then((category)=> {
+    
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const destoryCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    
+    if(!destoryCategory) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+    res.status(200).json(destoryCategory);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
